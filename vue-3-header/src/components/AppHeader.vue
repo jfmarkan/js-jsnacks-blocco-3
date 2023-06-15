ff4b9b3e25fa4a4afda0d700889d48f11c2516a5
jfmarkan <jfmarkan@gmail.com>
2023-06-15 12:44:10 +0200

    created structure and links in an Array of objects

---
+++ vue-3-header/src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="link in navLinks">
+                {{link.text}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            navLinks: [
+                {
+                    text: 'Home',
+                    link: '#',
+                },
+                {
+                    text: 'Contact',
+                    link: '#',
+                },
+                {
+                    text: 'Our Company',
+                    link: '#',
+                },
+                {
+                    text: 'Login',
+                    link: '#',
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
